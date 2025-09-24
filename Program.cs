48629e622dcad5c714fb7ac234a3839e8430b856
nadinaallves <nadinaallves@gmail.com>
2025-09-24 19:26:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Projeto_Inverter_Nome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome1, nome2, nome3, nome4, auxiliar;
+            Console.Write("Digite o nome#1: "); 
+            nome1 = Console.ReadLine();
+
+            Console.Write("Digite o nome#2: ");
+            nome2 = Console.ReadLine();
+
+            Console.Write("Digite o nome#3: ");
+            nome3 = Console.ReadLine();
+
+            Console.Write("Digite o nome#4: ");
+            nome4 = Console.ReadLine();
+
+            //Mecanismo para inverter os nomes
+            auxiliar = nome1;
+            nome1 = nome4;
+
+            nome4 = auxiliar;
+
+            auxiliar = nome2;
+            nome2 = nome3;
+
+            nome3 = auxiliar;
+
+
+            Console.WriteLine("Nomes inseridos na sequência Invertida:");
+            Console.WriteLine(nome1); 
+            Console.WriteLine(nome2);
+            Console.WriteLine(nome3);
+            Console.WriteLine(nome4);
+
+            Console.ReadKey(); // Aguarda o usuário pressionar uma tecla antes de fechar o console
+
+        }
+    }
+}
